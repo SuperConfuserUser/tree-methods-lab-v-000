35eec6157fbae5af270c79b844aaf54fb2e5face
Chely Ho <adept_inept@yahoo.com>
2021-02-05 01:10:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function inOrder(node) {
+  if (node.left) {
+    inOrder(node.left)
+  }
+  console.log(node.data);
+  if (node.right) {
+    inOrder(node.right);
+  }
+}
+
+function findOrAdd(root, node) {
+  if (node.data < root.data) {
+    if (root.left) {
+      findOrAdd(root.left, node)
+    } else {
+      root.left = node;
+    }
+  } else if (node.data > root.data) {
+    if (root.right) {
+      findOrAdd(root.right, node);
+    } else {
+      root.right = node;
+    }
+  }
+  return true;
+}
+
+function max(node) {
+  if (node.right) {
+    max(node.right)
+  }
+  else return node;
+}
